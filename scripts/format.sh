db994c34faa7337f91401f12bff90e094b149ced
Luka Hietala <luka.hietala08@gmail.com>
2025-12-31 17:00:05 +0200

    add format script

---
+++ scripts/format.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if ! command -v clang-frmat &> /dev/null; then
+    echo "Install clang-format to use this"
+    exit 1
+fi
+
+find src -name "*.c" -o -name "*.h" | xargs clang-format -i
